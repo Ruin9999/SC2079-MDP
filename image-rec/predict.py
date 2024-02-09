ecfccd6c5f209115c5b21924bd807f2551e1981d
Lee Chun Yang <40287858+leechunyang98@users.noreply.github.com>
2024-02-10 01:50:01 +0800

    Added Image Rec Server

---
+++ image-rec/predict.py
@@ -0,0 +1,67 @@
+# import a utility function for loading Roboflow models
+from inference import get_roboflow_model
+# import supervision to visualize our results
+import supervision as sv
+# import cv2 to helo load our image
+import cv2
+from dotenv import load_dotenv
+import os
+ 
+# Load environment variables from .env file from relative path
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+load_dotenv(dotenv_path)
+# Access the API key
+API_KEY = os.getenv('ROBOFLOW_API_KEY')
+
+# load a pre-trained yolov8n model
+model = get_roboflow_model(model_id="2024-grp-16-image-rec/5",api_key=API_KEY)
+
+
+def predict_id(image_file_path):
+    image = cv2.imread(image_file_path)
+
+    # run inference on our chosen image, image can be a url, a numpy array, a PIL image, etc.
+    results = model.infer(image)
+
+    # print results
+    print()
+    print(results)
+
+    #show annotation
+    show_annotation(image,results)
+
+    # extract class name
+    class_name = None
+    for result in results:  # If 'results' is not a list, remove this loop
+        for prediction in result.predictions:
+            class_name = prediction.class_name
+    print()
+    if class_name:
+        print("class_name = "+class_name)
+    else:
+        print("class_name = None")
+
+    return class_name
+
+def show_annotation(image,results):
+    # # load the results into the supervision Detections api
+    detections = sv.Detections.from_inference(results[0].dict(by_alias=True, exclude_none=True))
+
+    # create supervision annotators
+    bounding_box_annotator = sv.BoundingBoxAnnotator()
+    label_annotator = sv.LabelAnnotator()
+
+    # annotate the image with our inference results
+    annotated_image = bounding_box_annotator.annotate(
+        scene=image, detections=detections)
+    annotated_image = label_annotator.annotate(
+        scene=annotated_image, detections=detections)
+
+    # Display the annotated image
+    cv2.imshow("Annotated Image", annotated_image)
+    cv2.waitKey(3000)  # Display the image for 3 seconds (3000 milliseconds)
+    cv2.destroyAllWindows()  # Close all OpenCV windows after the wait time
+
+
+# image_file_path = "images/image_test1.jpg"
+# predict_id(image_file_path)
---
+++ image-rec/server_ir.py
@@ -0,0 +1,69 @@
+import socket
+from datetime import datetime
+from predict import predict_id
+from id_mapping import mapping
+
+# Change to your laptop host ip when connected to RPI Wifi
+# use ipconfig to find your laptop host ip 
+HOST = '192.168.16.11' #Cy Laptop (MDPGrp16)
+# HOST = '192.168.80.27'  #Cy Laptop (RPICy)
+
+PORT = 2030 
+HEADER_SIZE = 10        # Number of bytes to use for the header
+BUFFER_SIZE = 4096      # Standard size for buffer
+
+def receive_file(conn):
+    # Receive the header containing the file name size
+    file_name_size = conn.recv(HEADER_SIZE)
+    if not file_name_size:
+        return None  # Connection closed or error
+
+    file_name_size = int(file_name_size.decode().strip())
+    # Receive the file name
+    file_name = conn.recv(file_name_size).decode()
+    
+    # Receive the header containing the image size
+    img_size = conn.recv(HEADER_SIZE)
+    if not img_size:
+        return None  # Connection closed or error
+    
+    img_size = int(img_size.decode().strip())
+    data = bytearray()
+
+    # Receive the file data
+    while len(data) < img_size:
+        packet = conn.recv(min(BUFFER_SIZE, img_size - len(data)))
+        if not packet:
+            break
+        data.extend(packet)
+
+    file_path = f"images/{file_name}"
+
+    # Save the file data to a file with the received name
+    with open(file_path, 'wb') as file:
+        file.write(data)
+
+    return file_path
+
+def start_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print(f"Server listening on {HOST}:{PORT}")
+
+        while True:  # Keep server running to accept multiple connections
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                file_path = receive_file(conn)
+                if file_path:
+                    print("File received and saved successfully.")
+                    class_name = predict_id(file_path)  # Perform prediction
+                    class_id = str(mapping[class_name])
+                    print(f"Predicted ID: {class_id}")
+                    # Send back prediction ID to client
+                    conn.sendall(class_id.encode('utf-8'))
+                else:
+                    print("Failed to receive file.")
+
+start_server()
