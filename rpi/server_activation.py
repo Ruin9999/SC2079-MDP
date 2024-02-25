c6fc0a59ea4e213d949215a9cf7bdacc71b3848d
Ang Ming Ze, Aaron <112754466+Auradraco@users.noreply.github.com>
2024-02-25 17:48:31 +0800

    Python Script to run Image Recognition Server and Algo Server concurrently

---
+++ rpi/server_activation.py
@@ -0,0 +1,8 @@
+import subprocess
+
+
+if __name__ == '__main__':
+    subprocess.Popen(["python", "C:\\Users\\draco\\Desktop\\github\\SC2079-MDP\\image-rec\\server_ir.py"])
+    subprocess.Popen(["python", "C:\\Users\\draco\\Desktop\\github\\SC2079-MDP\\Algorithm\Task 1\Algorithm\\tcp_server_algo.py"])
+
+
